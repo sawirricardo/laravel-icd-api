3087862d072e214cbcf5b39b2a6f697cd9ba0eed
sawirricardo <sawir.ricardo@gmail.com>
2022-06-01 15:09:48 +0700

    add test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,25 +1,11 @@
 <?php
 
-use Mockery\MockInterface;
-use function Pest\Laravel\mock;
-use Sammyjo20\Saloon\Clients\MockClient;
-use Sammyjo20\Saloon\Http\MockResponse;
-use Sawirricardo\IcdApi\Connectors\AccessTokenConnector;
 
-use Sawirricardo\IcdApi\Laravel\IcdApi;
+use Sawirricardo\IcdApi\IcdApi;
+use Sawirricardo\IcdApi\Laravel\Facades\IcdApi as FacadesIcdApi;
 
 it('can test', function () {
-    $client = new MockClient([
-        MockResponse::make([
-            'icdaccessmanagement.who.int/*' => [
-                'access_token' => '1234567890',
-                'expires_in' => 3600,
-                'token_type' => 'Bearer',
-            ],
-        ]),
-    ]);
-    mock(AccessTokenConnector::class, function (MockInterface $mock) use ($client) {
-        $mock->shouldReceive('send')->with($client);
-    });
+    FacadesIcdApi::shouldReceive('client')
+        ->once();
     expect(icdApiClient())->toBeInstanceOf(IcdApi::class);
 });
